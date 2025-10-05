c0cb15721e4fe62d2faf6147ca1375c4704cd65f
dariusnafar <DariusSafavy@gmail.com>
2025-10-05 16:43:30 -0400

    remove the api from the input and read it from .env file

--- Agent/llm.py
+++ Agent/llm.py
@@ -1,9 +1,13 @@
 from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 class LLM:
-    def __init__(self,test_run=True, api_key=""):
+    def __init__(self,test_run=True):
         self.test_run = test_run
-        self.api_key = api_key
+        self.api_key = os.environ["OPENAI_API_KEY"]
         if self.test_run:
             self.swe_llm = ChatOpenAI(model="gpt-4o-mini", temperature=0, api_key=self.api_key)
             self.reviewer_llm = ChatOpenAI(model="gpt-4o-mini", temperature=0, api_key=self.api_key)
