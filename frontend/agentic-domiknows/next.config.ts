b1fbd47b69196333349070c545fbf6c4c638ec5e
dariusnafar <DariusSafavy@gmail.com>
2025-10-08 14:24:08 -0400

    Update nodejs configs

--- frontend/agentic-domiknows/next.config.ts
+++ frontend/agentic-domiknows/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  allowedDevOrigins: ['hlr-demo.egr.msu.edu', '*.hlr-demo.egr.msu.edu'],
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+
