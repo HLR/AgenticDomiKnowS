83b17ea8a1d46552b2373fd973c3c0f0f5a0ddf1
dariusnafar <DariusSafavy@gmail.com>
2025-11-18 18:30:47 -0500

    disable the graph visualization

--- Agent/Graph/graph_agent.py
+++ Agent/Graph/graph_agent.py
@@ -33,8 +33,8 @@ def graph_swe_agent(llm, Task_definition, rag_selected, graph_code_draft, graph_
 def graph_exe_agent(graph_code,task_id,len_code_list):
     code = extract_python_code(graph_code)
     captured_prints, captured_stderr, captured_error, graph = exec_code(code,return_graph=True)
-    if graph is not None:
-        graph.visualize(f"graph_images/{task_id}_{len_code_list}.png")
+    #if graph is not None:
+    #    graph.visualize(f"graph_images/{task_id}_{len_code_list}.png")
     return captured_error
 
 def graph_reviewer_agent(llm, Task_definition, code, rag_selected):
